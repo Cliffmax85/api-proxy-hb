e0b5f898037c9a9f8693ead286f573e75952ceb0
cliffmax85 <cliffmax85@gmail.com>
2022-02-14 17:21:56 -0800

    pokemon.js and some pokemon search

--- src/PokemonSearch.js
+++ src/PokemonSearch.js
@@ -2,12 +2,20 @@ import { useState } from 'react';
 
 export default function PokemonSearch() {
       // you'll need to track your pokemon search results, the loading state, and one form field: name. For this form field, set a real initial values (like 'pikachu') so the form populates with a default value.
+  const [loading, setLoading] = useState(false);
+  const [pokemon, setPokemon] = useState([]);
+  const [formName, setFormName] = useState('pikachu');
+  const [search, setSearch] = useState('');
+
+      
   
   async function handlePokemonSubmit(e) {
     e.preventDefault();
+    setLoading(true);
       
         // set the loading state to true
         // use fetch to make a request to your netlify pokemon function. Be sure to pass the pokemon name as a query param in the URL
+    const response = await   
   
         // put the jsonified data in state and set the loading state to false
   }
@@ -16,7 +24,7 @@ export default function PokemonSearch() {
     <section className='pokemon'>
       {/* make the fetch on submit */}
       <form>
-            Search pokemon for a city
+            Choose That Pokemon!
         {/* add inputs/labels for city name, state, and country, using all the things we need with react forms. Don't forget to use the value property to sync these up with the default values in react state */}
         <button>Get pokemon</button>
       </form>
