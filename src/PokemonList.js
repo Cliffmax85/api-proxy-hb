76bdc43e78cf31508d94d62787a50d7adc21cf14
cliffmax85 <cliffmax85@gmail.com>
2022-02-16 10:52:17 -0800

    fixed some formatting

--- src/PokemonList.js
+++ src/PokemonList.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function PokemonList({ pokemon }) {
   return (
-    <div> 
+    <div className='pokemon-detail'> 
       {pokemon.map((poke, i) => <div key={`${poke}-${i}`}>
         <p>{poke.pokemon}</p>
         <img src={poke.url_image} />
