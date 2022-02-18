0e5fca41f283bd4442b6894c09af727301e213c2
cliffmax85 <cliffmax85@gmail.com>
2022-02-18 15:17:19 -0800

    fixed misplaced ./

--- src/YelpSearch.js
+++ src/YelpSearch.js
@@ -1,13 +1,12 @@
 import { useState } from 'react';
 import Spinner from './Spinner';
+import BusinessList from './BusinessList';
 
 export default function YelpSearch() {
     // you'll need to track your yelp search results, the loading state, and a form field for location with a default value.
   const [loading, setLoading] = useState(false);
-  const [formCity, setFormCity] = useState('Astoria');
-  const [formState, setFormState] = useState('Oregon');
-  const [formCountry, setFormCountry] = useState('');
-  const [search, setSearch] = useState([]);
+  const [businesses, setBusinesses] = useState([]);
+  const [search, setSearch] = useState('');
 
   async function handleYelpSubmit(e) {
     e.preventDefault();
@@ -15,12 +14,13 @@ export default function YelpSearch() {
     // set the loading state to true
     setLoading(true);
     // use fetch to make a request to your netlify yelp function. Be sure to pass the search query as a query param in the URL
-    const response = await fetch(`./netlify/functions/yelp?city=${formCity}&state=${formState}&country=${formCountry}`);
+    const response = await fetch(`/.netlify/functions/yelp?search=${search}`);
   
     // put the jsonified data in state and set the loading state to false
     const json = await response.json();
+    setBusinesses(json);
+
     setLoading(false);
-    setSearch(json);
   }
   
   return (
@@ -29,20 +29,12 @@ export default function YelpSearch() {
       <form onSubmit={handleYelpSubmit}>
         Search yelp for a city
         {/* add inputs/labels for city name, state, and country, using all the things we need with react forms. Don't forget to use the value property to sync these up with the default values in react state */}
-        <label> City
-          <input required value={formCity} onChange={e => setFormCity(e.target.value)} />
-        </label>
-        <label> State
-          <input required value={formState} onChange={e => setFormState(e.target.value)} />
-        </label>
-        <label> Country 
-          <input required value={formCountry} onChange={e => setFormCountry(e.target.value)} />
-        </label>
+        <input required value={search} onChange={e => setSearch(e.target.value)} />
         <button>Search yelp</button>
       </form>
       {/* Make a BusinessesList component to import and use here. Use a ternery to display a loading spinner (make a <Spinner /> component for this) if the data is still loading. */}
       {
-        loading ? <Spinner /> : <BusinessesList businesses={search} />
+        loading ? <Spinner /> : <BusinessList businesses={businesses} />
       }
     </section>
   );
